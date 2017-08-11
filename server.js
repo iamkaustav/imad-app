9d0581777fe765b27813f6428990240c1c0dc116
iamkaustav <i.am.kaustav@gmail.com>
2017-08-11 22:21:43 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -13,6 +13,11 @@ app.get('/first', function (req, res) {
   res.send('THIS IS ARTICLE ONE');
 });
 
+app.get('/second', function (req, res) {
+  res.sendFile(path.join(__dirname, 'KAUSTAV.jpg'));
+});
+
+
 
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
